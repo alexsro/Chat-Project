ce4251fc08706e5df111cf658e5da92e00afd502
alex <alex000sander@gmail.com>
2022-08-04 22:29:32 -0300

    implementation of authenticate user service test

---
+++ Auth/src/modules/users/services/fakes/FakeAuthenticateUserService.spec.ts
@@ -0,0 +1,149 @@
+import AppError from "@shared/errors/AppError";
+
+import FakeUsersRepository from "@modules/users/repositories/fakes/FakeUsersRepository";
+import FakeHashProvider from "@modules/users/providers/HashProvider/fakes/FakeHashProvider";
+import FakeTokenProvider from "@modules/users/providers/TokenProvider/fakes/FakeTokenProvider";
+import FakeRefreshTokenRepository from "@modules/users/repositories/fakes/FakeRefreshTokenRepository";
+import AuthenticateUserService from '../AuthenticateUserService';
+import CreateUserService from '../CreateUserService'
+import dayjs from "dayjs";
+
+let fakeUserRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let fakeTokenProvider: FakeTokenProvider;
+let fakeRefreshTokenRepository: FakeRefreshTokenRepository;
+let authenticateUser: AuthenticateUserService;
+let createUser: CreateUserService;
+
+
+describe('AuthenticateUser', () => {
+  beforeEach(() => {
+    fakeUserRepository = new FakeUsersRepository;
+    fakeHashProvider = new FakeHashProvider;
+    fakeTokenProvider = new FakeTokenProvider;
+    fakeRefreshTokenRepository = new FakeRefreshTokenRepository;
+    authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+      fakeTokenProvider,
+      fakeRefreshTokenRepository
+    );
+    createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider
+    )
+  });
+
+  it('Should be able to authenticate a valid user and generate a new refresh_token', async () => {
+    const name = 'User1';
+    const email = 'alex000sander@gmail.com'
+    const password = '123456'
+
+    await createUser.execute({
+      name,
+      email,
+      password
+    });
+
+    const authenticatedUser = await authenticateUser.execute({
+      email,
+      password
+    });
+
+    expect(authenticatedUser).toHaveProperty("access_token")
+  });
+
+  it('Should be able to authenticate a valid user and return a existent refresh token', async () => {
+    const name = 'User1';
+    const email = 'alex000sander@gmail.com'
+    const password = '123456'
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password
+    });
+
+    const expiresIn = dayjs().add(7, "d").unix()
+    await fakeRefreshTokenRepository.create({
+      expiresIn,
+      userId: user.id
+    });
+
+    const authenticatedUser = await authenticateUser.execute({
+      email,
+      password
+    });
+
+    expect(authenticatedUser).toHaveProperty("access_token")
+  });
+
+  it('Should be able to authenticate a valid user, delete an expired refresh token and generate a new one', async () => {
+    const name = 'User1';
+    const email = 'alex000sander@gmail.com'
+    const password = '123456'
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password
+    });
+
+    const expiresIn = dayjs().add(1, "ms").unix()
+    await fakeRefreshTokenRepository.create({
+      expiresIn,
+      userId: user.id
+    });
+
+    const authenticatedUser = await authenticateUser.execute({
+      email,
+      password
+    });
+
+    expect(authenticatedUser).toHaveProperty("access_token")
+  });
+
+
+  it('Should be able to authenticate a valid user', async () => {
+    const name = 'User1';
+    const email = 'alex000sander@gmail.com'
+    const password = '123456'
+
+    await createUser.execute({
+      name,
+      email,
+      password
+    });
+
+    const authenticatedUser = await authenticateUser.execute({
+      email,
+      password
+    });
+
+    expect(authenticatedUser).toHaveProperty("access_token")
+  });
+
+  it("Should not be able to authenticate a invalid email", async () => {
+    await expect(authenticateUser.execute({
+      email: 'teste',
+      password: '1'
+    })).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should not be able to authenticate a valid email but invalid password", async () => {
+    const name = 'User1';
+    const email = 'alex000sander@gmail.com'
+    const password = '123456'
+
+    await createUser.execute({
+      name,
+      email,
+      password
+    });
+
+    await expect(authenticateUser.execute({
+      email,
+      password: '1'
+    })).rejects.toBeInstanceOf(AppError);
+  });
+});
